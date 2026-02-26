ebb1e2d40523a0f5889b8c26e2270b469d8e1322
Varun Singh <varun307.singh@gmail.com>
2026-02-26 15:23:12 +0530

    Add files via upload

---
+++ p1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    position VARCHAR(50) NOT NULL,
+    salary NUMERIC(10, 2) NOT NULL,
+    hire_date DATE NOT NULL
+);
